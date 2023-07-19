2c19e5aec31dd9b66272b911655c7f3d782fd77c
Christoph Blumschein <christoph.blumschein@student.hpi.uni-potsdam.de>
2023-07-20 00:11:28 +0200

    Refactor main

--- sonification/data-collection/main.py
+++ sonification/data-collection/main.py
@@ -5,26 +5,34 @@ import subprocess
 import os
 
 
-
 def run_program():
     input = open("/home/christoph/Uni/HPI/WiSe2223/ACP2/4-tree/ipomc10/large.in", "r")
     output = open("/dev/null", "w")
-    p2 = subprocess.Popen("/home/christoph/Uni/HPI/WiSe2223/ACP2/4-tree/ipomc10/cmake-build-debug/ipomc10", shell=False,
+    p2 = subprocess.Popen("/home/christoph/Uni/HPI/WiSe2223/ACP2/4-tree/ipomc10/cmake-build-release/ipomc10", shell=False,
                           stdin=input, stdout=output)
     pid = p2.pid
+
     def wait_for_exit():
         p2.wait()
 
     threading.Thread(target=wait_for_exit).start()
     return pid
 
+
 def get_pids_of_program(name: str):
-    return [int(pstr) for pstr in subprocess.run(["pidof", "-d,", name], stdout=subprocess.PIPE, text=True).stdout.split(',') if len(pstr) > 0]
+    return [int(pstr) for pstr in
+            subprocess.run(["pidof", "-d,", name], stdout=subprocess.PIPE, text=True).stdout.split(',') if
+            len(pstr) > 0]
+
+
+#pids = get_pids_of_program("firefox")
+#pids = [22895]
+#pids = get_pids_of_program("spotify")
+pids = [51542]
+#pids = get_pids_of_program("nautilus")
+
 
 #pids = [run_program()]
-#pids = [int(pstr) for pstr in subprocess.run(["pidof", "-d,", "firefox"], stdout=subprocess.PIPE, text=True).stdout.split(',') if len(pstr) > 0]
-#pids += [13640]
-pids = get_pids_of_program("spotify")
 
 OSC_SERVER_ADDRESS = os.getenv("OSC_SERVER_ADDRESS") or "localhost"
 OSC_SERVER_PORT = int(os.getenv("OSC_SERVER_PORT") or 9000)
@@ -35,10 +43,14 @@ client = udp_client.SimpleUDPClient(OSC_SERVER_ADDRESS, OSC_SERVER_PORT)
 from procfs_io import record_procfs_io
 from perf import record_perf
 
-threads = [threading.Thread(target=fun, args=(client,pids,INTERVAL_MS)) for fun in [record_procfs_io, record_perf]]
+recording_funcs = [
+    record_procfs_io,
+    record_perf
+]
+
+threads = [threading.Thread(target=fun, args=(client, pids, INTERVAL_MS)) for fun in recording_funcs]
 
 for t in threads:
     t.start()
 for t in threads:
     t.join()
-
